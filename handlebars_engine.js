89b4a365c8a007f8078243b33814461500160dbd
Michael Wolbert <michael@stamkracht.com>
2014-10-31 08:48:36 +0100

    Added registerPartial Added options for partial and helper registration Removed delimiters (seems like a bug in the package; not all expressions were evaluated)

--- handlebars_engine.js
+++ handlebars_engine.js
@@ -1,34 +1,41 @@
 var Handlebars = require('handlebars'),
-	handlebarsDelimiters = require('handlebars-delimiters'),
-    _ = require('underscore.string');
+  handlebarsDelimiters = require('handlebars-delimiters'),
+  fs = require('fs');
 
-// register underscore.string helpers
-Object.keys(_).forEach(function(helper) {
-	var _sHelper = _[helper];
-	if(Handlebars.Utils.isFunction(_sHelper))
-		Handlebars.registerHelper(helper, function() {
-			var args = Array.prototype.slice.call(arguments, 0).slice(0, -1);
-			return _sHelper.apply(this, args);
-		});
-});
-
-var engine = module.exports = function (beginDelimiter, endDelimiter) {
-  var me = this;
-
-  this.setDelimiter = function(beginDelimiter, endDelimiter) {
-    me._beginDelimiter = beginDelimiter || '{{';
-    me._endDelimiter = endDelimiter || '}}';
-    handlebarsDelimiters(Handlebars, [me._beginDelimiter, me._endDelimiter]);
-    return me.engine;
-  };
-
+module.exports = function(options) {
   this.engine = function (source, data) {
     return Handlebars.compile(source)(data);
   };
 
-  this.engine.detect = function(body) {
-    return body.indexOf(me._beginDelimiter) > -1;
-	};
+  this.engine.detect = function (body) {
+    return body.indexOf('{{') > -1;
+  };
 
-  return this.setDelimiter(beginDelimiter, endDelimiter);
+  // register underscore.string helpers
+  options.fns = options.fns || require('underscore.string');
+
+  var registerHelpers = function (fns) {
+    Object.keys(fns)
+      .filter(function (fnName) {
+        return Handlebars.Utils.isFunction(fns[fnName]);
+      })
+      .forEach(function (fnName) {
+        Handlebars.registerHelper(fnName, function () {
+          var args = Array.prototype.slice.call(arguments, 0).slice(0, -1);
+          return fns[fnName].apply(this, args);
+        });
+      });
+  };
+
+  var registerPartials = function (partialsPath) {
+    fs.readdirSync(partialsPath).forEach(function (p) {
+      var template = fs.readFileSync(partialsPath + p, 'utf-8');
+      console.log(partialsPath + p);
+      Handlebars.registerPartial(p, template);
+    });
+  };
+
+  if(options && options.fns) registerHelpers(options.fns);
+  if(options && options.partials) registerPartials(options.partials);
+  return this.engine;
 };
